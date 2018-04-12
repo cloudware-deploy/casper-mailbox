31027434d7099f51acdaeee88704c2be3120b1c7
Tiago Dias <tiago.dias@cldware.com>
2018-04-12 11:18:50 +0100

    Initial commit

---
+++ lib/casper/mailbox.rb
@@ -0,0 +1,7 @@
+require "casper/mailbox/version"
+
+module Casper
+  module Mailbox
+    # Your code goes here...
+  end
+end
---
+++ lib/casper/mailbox/version.rb
@@ -0,0 +1,5 @@
+module Casper
+  module Mailbox
+    VERSION = "0.1.0"
+  end
+end
